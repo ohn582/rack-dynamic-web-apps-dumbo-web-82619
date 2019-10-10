0f7e7637eb6258fd8cf262e32d39b6eafe4c8f3f
Neal JIn Oh <no4259@gmail.com>
2019-10-10 17:59:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,10 @@ class Application
   num_2 = Kernel.rand(1..20)
   num_3 = Kernel.rand(1..20)
  
- 
+ if num_1==num_2 && num_2==num_3
+    puts "You Win"
+  else
+    puts "You Lose"
+  end
 
 end
