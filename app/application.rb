0d590b8e7d9e4c5e92a93b65d1bcb580fbfe2436
Neal JIn Oh <no4259@gmail.com>
2019-10-10 18:02:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,15 +6,19 @@ class Application
     resp.finish
   end
   
+  def call(env)
+    resp = Rack::Response.new
   
-  num_1 = Kernel.rand(1..20)
-  num_2 = Kernel.rand(1..20)
-  num_3 = Kernel.rand(1..20)
- 
- if num_1==num_2 && num_2==num_3
-    puts "You Win"
-  else
-    puts "You Lose"
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+   
+    if num_1==num_2 && num_2==num_3
+      resp.write  "You Win"
+    else
+      resp.write  "You Lose"
+    end
+    resp.finish
   end
 
 end
